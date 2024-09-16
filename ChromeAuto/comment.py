043eeadcbe1b83ec9be1319cc2fab6c5a30a4ce4
富强民主文明和谐 <86072267+s1wei@users.noreply.github.com>
2024-09-16 20:05:22 +0800

    Update and rename comment.py to ChromeAuto/comment.py

--- ChromeAuto/comment.py
+++ ChromeAuto/comment.py
@@ -2,13 +2,6 @@
 # _*_ coding: utf-8 _*_
 """
 Copyright (C) 2024 - s1wei.com, Inc. All Rights Reserved 
-
-@Time    : 2024/8/2 下午5:18
-@Author  : s1wei
-@Email   : admin@s1wei.com
-@Blog    : https://www.denceun.cn/author/1/
-@File    : comment.py.py
-@IDE     : PyCharm
 """
 
 
