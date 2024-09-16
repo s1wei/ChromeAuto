2d28d824b1bf5ed8c6f13ae41d5a711b5ffdd113
富强民主文明和谐 <86072267+s1wei@users.noreply.github.com>
2024-09-16 20:04:42 +0800

    Update and rename __init__.py to ChromeAuto/__init__.py

---
+++ ChromeAuto/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+"""
+Copyright (C) 2024 - s1wei.com, Inc. All Rights Reserved 
+"""
+
+from .initiate import ChromeInit
+from .comment import Tab
+# from .manner import BrowserManager
+# print("Copyright (c) 2024 - s1wei.com, Inc. All Rights Reserved")
